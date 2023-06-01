e5a56f35a64ad9bbacdb63e101375b68e6e8e1bb
utoniumharsha <sriharsha.vipparti@gmail.com>
2023-05-31 19:31:46 -0500

    Add base server and client code

---
+++ src/server.cpp
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
